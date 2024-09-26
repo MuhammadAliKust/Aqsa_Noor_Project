20b394750149c2c92c6360161c3fb6b0cbc5d456
Muhammad Ali Nizami <kustalics@gmail.com>
2024-09-26 14:42:44 +0500

    Row, Column Done

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.green,
         appBar: AppBar(
           backgroundColor: Colors.blue,
           title: Text(
@@ -19,11 +20,32 @@ class MyApp extends StatelessWidget {
                 color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
           ),
           centerTitle: true,
-          leading: Icon(Icons.arrow_back,color: Colors.white,),
+          leading: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
           actions: [
-            Icon(Icons.notifications,color: Colors.white,),
-            Icon(Icons.shopping_cart,color: Colors.white,),
+            Icon(
+              Icons.notifications,
+              color: Colors.white,
+            ),
+            Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ),
           ],
+        ),
+        body: Row(
+          children: [
+            Text("First Text",style: TextStyle(fontSize: 20),),
+            SizedBox(height: 30,),
+            Text("Second Text",style: TextStyle(fontSize: 20),),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          onPressed: () {},
+          child: Icon(Icons.add),
         ),
       ),
     );
