455f521621e207c99735f6d8e2bd9aaf12b770b4
Muhammad Ali Nizami <kustalics@gmail.com>
2024-10-04 19:30:30 +0500

    Asset Image and Network Image Done

---
+++ lib/asset_image.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AssetsImageView extends StatelessWidget {
+  const AssetsImageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Asset Image"),
+      ),
+      body: Image.asset('assets/images/1.png'),
+    );
+  }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:aqsa_noor_project/asset_image.dart';
 import 'package:flutter/material.dart';
+
+import 'network_image.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,64 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.green,
-        appBar: AppBar(
-          backgroundColor: Colors.blue,
-          title: Text(
-            "First Screen",
-            style: TextStyle(
-                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          leading: Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-          actions: [
-            Icon(
-              Icons.notifications,
-              color: Colors.white,
-            ),
-            Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            Text(
-              "In publishing and graphic design, Lorem ipsum is a placeholder text ",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available",
-              style: TextStyle(fontSize: 17),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Icon(Icons.thumb_up),
-                Icon(Icons.favorite),
-                Icon(Icons.comment),
-                Icon(Icons.share)
-              ],
-            )
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue,
-          onPressed: () {},
-          child: Icon(Icons.add),
-        ),
-      ),
+      home: AssetsImageView()
     );
   }
 }
